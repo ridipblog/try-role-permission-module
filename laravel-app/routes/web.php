879d9c45ad4e4efe3ff4d18173f0b0d417b82fbc
ridipblog <memorytemp5@gmail.com>
2025-06-06 20:37:37 +0530

    re test after install by comoser

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -30,7 +30,7 @@ Route::group(['prefix' => 'v1', 'middleware' => ['buglock.auth:web,view,yes']], 
     Route::get('/dash-1', [TestController::class, 'dash_1']);
 });
 
-Route::group(['prefix' => 'v1', 'middleware' => ['buglock.auth:web,view,yes','buglock.role:web,view,admin-1,admin-21']], function () {
+Route::group(['prefix' => 'v1', 'middleware' => ['buglock.auth:web,view,no','buglock.role:web,view,admin-1,admin-21']], function () {
     Route::get('/dash-2', [TestController::class, 'dash_2']);
 });
 Route::group(['prefix' => 'v1', 'middleware' => ['buglock.permission:web,view,edit-easy-1,easy-deleter-1']], function () {
